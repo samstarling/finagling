42ba83a6fbe67891f7ef103bd3d1de672f905676
Sam Starling <mail@samstarling.co.uk>
2015-01-28 17:32:45 +0100

    Formatting

--- src/main/scala/com/samstarling/futures/ConcurrentComposition.scala
+++ src/main/scala/com/samstarling/futures/ConcurrentComposition.scala
@@ -4,9 +4,14 @@ import com.twitter.util.Future
 
 object ConcurrentComposition extends App {
 
-  def fetchTrack(): Future[String] = Future { "My Track" }
-  def findComments(track: String): Future[Seq[String]] = Future { List("Nice!", "Awesome!", "Terrible!") }
-  def sanitizeComment(comment: String): Future[String] = Future { comment.toLowerCase }
+  def fetchTrack(): Future[String] =
+    Future { "My Track" }
+  
+  def findComments(track: String): Future[Seq[String]] =
+    Future { List("Nice!", "Awesome!", "Terrible!") }
+  
+  def sanitizeComment(comment: String): Future[String] =
+    Future { comment.toLowerCase }
 
   val f: Future[Seq[String]] =
     fetchTrack() flatMap { content =>
