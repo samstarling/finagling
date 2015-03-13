47947f62b4cb36647116f080fbec02de3fcc358b
Sam Starling <sam@soundcloud.com>
2015-03-13 16:43:59 +0100

    Simplified server

--- src/main/scala/com/samstarling/server/MyServer.scala
+++ src/main/scala/com/samstarling/server/MyServer.scala
@@ -6,18 +6,12 @@ import com.twitter.finagle.Service
 import com.twitter.finagle.builder.ServerBuilder
 import com.twitter.finagle.http.Http
 import com.twitter.util.Future
+import org.jboss.netty.buffer.ChannelBuffers
 import org.jboss.netty.handler.codec.http.{DefaultHttpResponse, HttpRequest, HttpResponse, HttpResponseStatus, HttpVersion}
 
 object MyServer extends App {
 
-  val rootService = new Service[HttpRequest, HttpResponse] {
-    def apply(request: HttpRequest) = Future {
-      request.getUri match {
-        case "/" => new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK)
-        case _ => new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.NOT_FOUND)
-      }
-    }
-  }
+  val rootService = new HelloWorldService()
 
   val address = new InetSocketAddress(10000)
   val server = ServerBuilder()
@@ -25,4 +19,13 @@ object MyServer extends App {
     .bindTo(address)
     .name("HttpServer")
     .build(rootService)
+}
+
+class HelloWorldService extends Service[HttpRequest, HttpResponse] {
+  override def apply(request: HttpRequest): Future[HttpResponse] = Future {
+    val response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK)
+    val content = ChannelBuffers.copiedBuffer("Hello world", "UTF-8")
+    response.setContent(content)
+    response
+  }
 }
