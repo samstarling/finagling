51653bd9fa22c7c66ee3126f8411c7b0a93aa0ef
Sam Starling <sam@soundcloud.com>
2015-03-13 16:20:10 +0100

    Updated sequential composition example

--- src/main/scala/com/samstarling/futures/SequentialComposition.scala
+++ src/main/scala/com/samstarling/futures/SequentialComposition.scala
@@ -6,22 +6,29 @@ object SequentialComposition extends App {
 
   case class User(name: String)
 
-  def authenticate(credentials: String) = Future { Some(User("sam")) }
+  def authenticate(credentials: String) = Future { 
+    if (credentials == "correct") User(credentials)
+    else throw new RuntimeException("Invalid credentials")
+  }
 
-  def getTweets(user: Option[User]) = Future { List("1", "2", "3") }
+  def getTweets(user: User) = Future {
+    List("lol", "rofl", "lmao")
+  }
 
   // Here, the intermediate user object can't be accessed
   authenticate("foo").flatMap(getTweets) onSuccess { tweets =>
     println(s"Your tweets are: $tweets")
   }
-  
+
   // Here, we retain access to a val for each step
   val future = for {
-    user <- authenticate("foo")
+    user <- authenticate("correct")
     tweets <- getTweets(user)
   } yield (user, tweets)
 
   future onSuccess { case (user, tweets) =>
     println(s"The tweets for $user are: $tweets")
+  } onFailure { ex =>
+    println(s"Failure: $ex")
   }
 }
