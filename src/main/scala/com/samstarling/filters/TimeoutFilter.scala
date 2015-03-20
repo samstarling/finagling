fe65a55424c7b2cfe66478112d8c856d028010c2
Sam Starling <sam@soundcloud.com>
2015-03-20 15:21:59 +0100

    Simplified filter

--- src/main/scala/com/samstarling/filters/TimeoutFilter.scala
+++ src/main/scala/com/samstarling/filters/TimeoutFilter.scala
@@ -4,16 +4,13 @@ import com.twitter.finagle._
 import com.twitter.finagle.http.{Response, Request}
 import com.twitter.util.{Duration, Future, Timer}
 
-class TimeoutFilter(timeout: Duration, exception: RequestTimeoutException, timer: Timer) extends SimpleFilter[Request, Response] {
-  def this(timeout: Duration, timer: Timer) =
-    this(timeout, new IndividualRequestTimeoutException(timeout), timer)
-
+class TimeoutFilter(timeout: Duration, timer: Timer) extends SimpleFilter[Request, Response] {
   def apply(request: Request, service: Service[Request, Response]): Future[Response] = {
     val res = service(request)
     res.within(timer, timeout) rescue {
-      case _ =>
-        res.raise(exception)
-        Future.exception(exception)
+      case ex =>
+        res.raise(ex)
+        Future.exception(ex)
     }
   }
 }
