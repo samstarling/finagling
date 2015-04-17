85ff4e61257f805881148c64f884999e09517d70
Sam Starling <sam@soundcloud.com>
2015-04-17 14:48:33 +0200

    Fixed concurrent examples

--- src/main/scala/com/samstarling/futures/ConcurrentComposition.scala
+++ src/main/scala/com/samstarling/futures/ConcurrentComposition.scala
@@ -1,34 +1,52 @@
 package com.samstarling.futures
 
-import com.twitter.util.{Await, Future}
+import java.util.concurrent.Executors
+
+import com.twitter.util.{FuturePool, Future, Await}
 import com.samstarling.util.time
 
 object ConcurrentComposition extends App {
 
-  def slowTask(time: Int): Future[String] = {
+  val executor = Executors.newFixedThreadPool(2)
+  val futurePool = FuturePool(executor)
+
+  def slowTask(time: Int): Future[String] = futurePool {
     Thread.sleep(time)
-    Future.value("Hello")
+    "Hello"
   }
 
-  /* collect takes ? seconds */
-  time("Future.collect", {
-    Await.result(Future.collect(List(slowTask(1000), slowTask(2000))))
+  time("collect", {
+    val f = Future.collect(List(slowTask(1000), slowTask(2000)))
+    val result = Await.result(f)
+    println(s"Result: $result")
   })
 
-  /* join takes ? seconds */
-  time("Future.join", {
-    Await.result(Future.join(List(slowTask(1000), slowTask(2000))))
+  time("join", {
+    val f = Future.join(slowTask(1000), slowTask(2000))
+    val result = Await.result(f)
+    println(s"Result: $result")
   })
 
-  /* ? */
-  time("Future.join flatMap", {
-    Future.join(
-      slowTask(1000),
-      slowTask(2000)
-    ).flatMap { case (comments, adverts) =>
-      println(comments)
-      println(adverts)
-      Future.None
-    }
+  /* Careful: this takes 3 seconds total */
+  time("for", {
+    val f = for {
+      r1 <- slowTask(1000)
+      r2 <- slowTask(2000)
+    } yield (r1, r2)
+    val result = Await.result(f)
+    println(s"Result: $result")
   })
+
+  time("for - v2", {
+    val f1 = slowTask(1000)
+    val f2 = slowTask(2000)
+    val f = for {
+      r1 <- f1
+      r2 <- f2
+    } yield (r1, r2)
+    val result = Await.result(f)
+    println(s"Result: $result")
+  })
+
+  executor.shutdown()
 }
